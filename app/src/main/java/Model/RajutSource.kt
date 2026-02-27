0bcac322d9e4d3500199e636f7a767691f08c47a
FenyAdria <fenyadriam@gmail.com>
2026-02-27 16:44:27 +0700

    commit

---
+++ app/src/main/java/Model/RajutSource.kt
@@ -0,0 +1,10 @@
+package Model
+import com.example.praktam2_2417051010.R
+
+object RajutSource {
+    val dummyRajut = listOf(
+        Rajut(nama = "shaun", deskripsi = "rajutan", harga = 10000, ImageRes = R.drawable.Shaun),
+        Rajut(nama = "bear", deskripsi = "rajutan", harga = 10000, ImageRes = R.drawable.bear),
+        Rajut(nama = "paus", deskripsi = "rajutan", harga = 10000, ImageRes = R.drawable.Paus)
+        )
+}
--- app/src/main/java/com/example/praktam2_2417051010/MainActivity.kt
+++ app/src/main/java/com/example/praktam2_2417051010/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.praktam2_2417051010
 
+import Model.RajutSource
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.example.praktam2_2417051010.ui.theme.PrakTAM2_2417051010Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +28,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrakTAM2_2417051010Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Feny",
-                        npm = "2417051010",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
@@ -32,17 +35,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, npm:String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Halo, saya $name dengan NPM $npm siap belajar Compose!",
-        modifier = modifier
-    )
+fun Greeting() {
+    val Rajut = RajutSource.dummyRajut[0]
+
+    Column(modifier = Modifier.fillMaxSize().padding(all = 60.dp)) {
+        Image(
+            painter = painterResource(id = Rajut.ImageRes),
+            contentDescription = Rajut.nama,
+            modifier = Modifier.size(200.dp),
+            contentScale = ContentScale.Crop
+        )
+        Text(text = "Nama : ${Rajut.nama}")
+        Text(text = "Deskripsi : ${Rajut.deskripsi}")
+        Text(text = "Harga : ${Rajut.harga}")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PrakTAM2_2417051010Theme {
-        Greeting("Feny", npm = "2417051010")
+        Greeting()
     }
 }
